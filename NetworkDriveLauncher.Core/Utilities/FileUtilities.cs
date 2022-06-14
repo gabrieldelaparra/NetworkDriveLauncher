d03dfbf6d30cd1a370cfd08100a456354b293fd7
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2022-06-14 23:52:13 +0200

    Add some checks to see if the index file is currently in used/locked.

---
+++ NetworkDriveLauncher.Core/Utilities/FileUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace NetworkDriveLauncher.Core.Utilities
+{
+    public static class FileUtilities
+    {
+        public static bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
