f5c8163d72213a7c4b7cba58456655c9fab935ef
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2022-06-15 22:23:36 +0200

    Create base class. PlaintText implements this and adds the index filename.

---
+++ NetworkDriveLauncher.Core/Index/AppSettingsConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using NetworkDriveLauncher.Core.Models;
+using Wororo.Utilities;
+
+namespace NetworkDriveLauncher.Core.Index
+{
+    public class AppSettingsConfiguration : IIndexConfiguration
+    {
+        internal readonly IConfiguration Configuration;
+
+        protected AppSettingsConfiguration(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public bool OverwriteIndex => Configuration[nameof(OverwriteIndex)]?.ToBool() ?? true;
+        public int Depth => Configuration[nameof(Depth)]?.ToInt() ?? 3;
+        public IEnumerable<string> RootDirectories
+        {
+            get
+            {
+                var rootDirectory = Configuration
+                    .GetSection(nameof(RootDirectories))
+                    .GetChildren()
+                    .Select(x => x.Value)
+                    .ToList();
+                return rootDirectory;
+            }
+        }
+    }
+}
--- NetworkDriveLauncher.Core/Index/PlainTextIndexConfiguration.cs
+++ NetworkDriveLauncher.Core/Index/PlainTextIndexConfiguration.cs
@@ -1,34 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
-using Wororo.Utilities;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace NetworkDriveLauncher.Core.Index
 {
-    public class PlainTextIndexConfiguration : IIndexConfiguration
+    public class PlainTextIndexConfiguration : AppSettingsConfiguration
     {
-        private readonly IConfiguration _configuration;
-
-        public PlainTextIndexConfiguration(IConfiguration configuration)
+        public PlainTextIndexConfiguration(IConfiguration configuration) : base(configuration)
         {
-            _configuration = configuration;
         }
-
-        public bool OverwriteIndex => _configuration[nameof(OverwriteIndex)]?.ToBool() ?? true;
-        public int Depth => _configuration[nameof(Depth)]?.ToInt() ?? 3;
-        public IEnumerable<string> RootDirectories
-        {
-            get
-            {
-                var rootDirectory = _configuration
-                    .GetSection(nameof(RootDirectories))
-                    .GetChildren()
-                    .Select(x => x.Value)
-                    .ToList();
-                return rootDirectory;
-            }
-        }
-
-        public string OutputFilename => _configuration[nameof(OutputFilename)];
+        public string OutputFilename => Configuration[nameof(OutputFilename)];
     }
 }
